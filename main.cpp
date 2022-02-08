d2add3e47278bca8acfc7dfd527b6628f236e931
Luis Torres Cisneros <luis.torres.cisneros@sioux.eu>
2022-02-08 16:39:13 +0100

    Changes on message

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<< "Hello Docker Container on GitHub!"<<endl;
+    cout<< "Hello JFrog on GitHub!"<<endl;
 
     return 0;
 }
